47fab787cb4cc4d1ef331db1514647facae2a5b2
Adam Kona <adam.kona4@gmail.com>
2022-06-19 21:26:42 +0100

    Display notifications on dashboard and filter

--- src/Utils/databaseAccessor.js
+++ src/Utils/databaseAccessor.js
@@ -1,81 +1,103 @@
-const API_BASE = "https://deen-developers-team4.herokuapp.com"
+const API_BASE = "https://deen-developers-team4.herokuapp.com";
 
 const getAllUsers = async () => {
-  
-    const users =  await fetch(`${API_BASE}/user`).then(
-    (response) => response.json());
+  const users = await fetch(`${API_BASE}/user`).then((response) =>
+    response.json()
+  );
 
-    console.log(users);
+  console.log(users);
 
-    return users;
+  return users;
 };
 
 const getAllCamps = async () => {
-    const camps =  await fetch(`${API_BASE}/user`).then(
-        (response) => response.json()
-    );
-    return camps;
-}
+  const camps = await fetch(`${API_BASE}/user`).then((response) =>
+    response.json()
+  );
+  return camps;
+};
 
-const getAllProvisions = async() => {
-    const provisions =  await fetch("https://deen-developers-team4.herokuapp.com/provision").then(
-        (response) => response.json()
-    );
-    return provisions;
-}
+const getAllProvisions = async () => {
+  const provisions = await fetch(
+    "https://deen-developers-team4.herokuapp.com/provision"
+  ).then((response) => response.json());
+  return provisions;
+};
 
-const addProvisionToUser = async(jsonObj) => {
-    const rawResponse = await fetch("https://deen-developers-team4.herokuapp.com/provision/add", {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(jsonObj)
-      });
-      rawResponse.json().then(obj => {
-          console.log(obj)
-        const notifyJson = {
-            "familyId": obj.userId,
-            "provisionId": obj.provisionId,
-
-        }
-        addNotificationForPackage(notifyJson).then(notify => {
-            console.log("done")
-        })
-    })
-}
+const addProvisionToUser = async (jsonObj) => {
+  const rawResponse = await fetch(
+    "https://deen-developers-team4.herokuapp.com/provision/add",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jsonObj),
+    }
+  );
+  rawResponse.json().then((obj) => {
+    console.log(obj);
+    const notifyJson = {
+      familyId: obj.userId,
+      provisionId: obj.provisionId,
+    };
+    addNotificationForPackage(notifyJson).then((notify) => {
+      console.log("done");
+    });
+  });
+};
 
 const addNotificationForPackage = async (theData) => {
+  const rawResponse = await fetch(
+    "https://deen-developers-team4.herokuapp.com/notification",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(theData),
+    }
+  );
 
-    const rawResponse = await fetch("https://deen-developers-team4.herokuapp.com/notification", {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(theData)
-      });
-
-      rawResponse.json().then(obj => {
-        console.log("added notification", obj)
-      }).catch(error => {
-          console.log(error)
-      })
-}
+  rawResponse
+    .json()
+    .then((obj) => {
+      console.log("added notification", obj);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 const addUsers = async (users) => {
   const rawResponse = await fetch(`${API_BASE}/family`, {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(users)
-});
-const content = await rawResponse.json();
-console.log(content)
-return content
-}
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(users),
+  });
+  const content = await rawResponse.json();
+  console.log(content);
+  return content;
+};
 
-export {getAllUsers, getAllCamps,getAllProvisions,addProvisionToUser, addUsers};
+const getAllNotifications = async () => {
+  const notifications = await fetch(`${API_BASE}/notifications`).then(
+    (response) => response.json()
+  );
+  console.log(JSON.stringify(notifications));
+  return notifications;
+};
+
+export {
+  getAllUsers,
+  getAllCamps,
+  getAllProvisions,
+  addProvisionToUser,
+  addUsers,
+  getAllNotifications,
+};
